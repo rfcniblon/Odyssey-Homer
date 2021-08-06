09c132de0d4a9d3d938acd400a37c7d5e3fe6708
rfcniblon <lambinet.nicolas@gmail.com>
2021-08-06 23:36:56 +0200

    Account registration in database

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const parser = require("body-parser");
 const router = express.Router();
-const connection = require('../../helpers/db');
+const connection = require("../../helpers/db");
 
 router.use(parser.json());
-router.use(parser.urlencoded({
-    extended:true
-}));
+router.use(
+  parser.urlencoded({
+    extended: true,
+  })
+);
 
-router.post('/signup', function(req, res, next) {
-    const userSign = req.body;
+router.post("/signup", function(req,res, next) {
+    const user = req.body;
       connection.query(
-        "INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)", 
-        [userSign.email, userSign.password, userSign.name, userSign.lastname], 
+        "INSERT INTO users (email, password, name, lastname) VALUES (?, ?, ?, ?)", 
+        [user.email, user.password, user.name, user.lastname, ], 
         (error, results, fields) => {
           if( error ){
             res.status(500).end();
           }
           else{
-           res.status(200).end();
-        }
+            res.status(200).end();
+          }
         }
       )
-    });
+    }
+  )
 
-module.exports = router; 
+
+module.exports = router;
